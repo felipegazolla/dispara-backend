fb2ebcb1a1c9e1df60e61617c020d012be41e02f
felipe <enggazolla@gmail.com>
2025-01-13 17:10:25 -0300

    using AppError

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,12 @@
+import { AppError } from "../utils/AppError.js";
+
 export class UserController {
 	create(req, res) {
 		const { name, email, password } = req.body;
+
+		if (!name) {
+			throw new AppError("Nome obrigatório.");
+		}
 
 		res.status(201).json({ name, email, password });
 	}
--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
+import "express-async-errors";
+import { AppError } from "./utils/AppError.js";
 import express from "express";
 import { routes } from "./routes/index.js";
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+	if (error instanceof AppError) {
+		return res.status(error.statusCode).json({
+			status: "error",
+			message: error.message,
+		});
+	}
+
+	console.error(error);
+
+	return res.status(500).json({
+		status: "error",
+		message: "Internal server error",
+	});
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
